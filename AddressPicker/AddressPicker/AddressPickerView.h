140b63e4cadd8637c3ef2f827c8c0b8bc88be22f
quanyuanlin <quanyuanlin@126.com>
2017-09-14 17:01:25 +0800

    first commit

---
+++ AddressPicker/AddressPicker/AddressPickerView.h
@@ -0,0 +1,13 @@
+//
+//  AddressPickerView.h
+//  AddressPicker
+//
+//  Created by admin on 2017/9/14.
+//  Copyright © 2017年 xiaoyukeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddressPickerView : UIView
+
+@end
---
+++ AddressPicker/AddressPicker/AddressPickerView.m
@@ -0,0 +1,21 @@
+//
+//  AddressPickerView.m
+//  AddressPicker
+//
+//  Created by admin on 2017/9/14.
+//  Copyright © 2017年 xiaoyukeji. All rights reserved.
+//
+
+#import "AddressPickerView.h"
+
+@implementation AddressPickerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
